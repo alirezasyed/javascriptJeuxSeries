f5495b74c847fefb1dac94996a5bc6db8b5b3f3e
alirezasyed <codingchallenge2020@yahoo.com>
2021-06-19 12:38:43 -0700

    ajoute les premières lignes de code pour le jeu de tête ou de queue

---
+++ script.js
@@ -0,0 +1,16 @@
+const message = document.querySelector('.message');
+
+const buttons = document.querySelectorAll("button");
+
+console.log(buttons);
+
+for(let i = 0; i < buttons.length; i++)
+{
+    console.log(buttons[i]);
+    buttons[i].addEventListener("click", tossCoin);
+}
+
+function tossCoin(e)
+{
+    console.log(e.target.innerText);
+}
